de02e326c094429e1907d34bd51184e287599ed3
Poli Rodrigues <polifreire@gmail.com>
2022-04-11 12:12:44 -0300

    massa de dados em lista

--- cypress/integration/endereco.spec.js
+++ cypress/integration/endereco.spec.js
@@ -1,4 +1,6 @@
 /// <reference types="cypress"/>
+import EnderecoPage from '../support/page-objects/endereco.page'
+const dadosEndereco = require('../fixtures/endereco.json')
 
 describe('Funcionalidade Endereços - Faturamento e Entrega', () => {
     beforeEach(() => {
@@ -9,7 +11,27 @@ describe('Funcionalidade Endereços - Faturamento e Entrega', () => {
     });
 
     it('Deve fazer cadastro de faturamento com sucesso', () => {
+        EnderecoPage.editarEnderecoFaturamento('Lichia', 'Teste', 'Taylor Nation', 'Brasil', 'Av. Rio Branco', '1989', 'Recife', 'Pernambuco', '50030-900', '81900000000', 'email@email.com',)
+        cy.get('.woocommerce-message').should('contain' , 'Endereço alterado com sucesso.')
         
-        //cadastro de endereço
     });
+
+    it('Deve fazer cadastro de faturamento com sucesso - Usando arquivo de dados', () => {
+        EnderecoPage.editarEnderecoFaturamento(
+            dadosEndereco[1].nome,
+            dadosEndereco[1].sobrenome,
+            dadosEndereco[1].empresa,
+            dadosEndereco[1].pais,
+            dadosEndereco[1].endereco,
+            dadosEndereco[1].numero,
+            dadosEndereco[1].cidade,
+            dadosEndereco[1].estado,
+            dadosEndereco[1].cep,
+            dadosEndereco[1].telefone,
+            dadosEndereco[1].email
+            )
+        cy.get('.woocommerce-message').should('contain' , 'Endereço alterado com sucesso.')
+        
+    });
+
 });
